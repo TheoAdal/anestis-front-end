425f66d4d4455ada8e0c073f1a5580848e406c78
TheoAdal <theoadal@gmail.com>
2024-06-19 13:31:04 +0300

    scss changes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 import { BrowserRouter } from "react-router-dom";
 import TopBarNav from "./components/TopBar/TopBarNav";
 import FooterWrapper from "./components/Footer/FooterWrapper";
+import ContentWrapper from "./components/ContentWrapper/ContentWrapper";
 
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <div className="top-bar-container">
-           <TopBarNav />
+      <div className="App">
+        <div className="top-bar-container">
+          <TopBarNav />
         </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        > 
-          Learn React
-        </a>
-      </header>
-      <div className="footer-container">
-           <FooterWrapper />
-         </div>
-    </div>
+        <div className="content-wrapper">
+          <ContentWrapper /> 
+        </div>
+        <div className="footer-container">
+          <FooterWrapper />
+        </div>
+      </div>
     </BrowserRouter>
-  
   );
 }
 
 export default App;
 
 //   <BrowserRouter>
-  //   <div className="App">
-  //       <div className="top-bar-container">
-  //         <TopBarNav />
-  //       </div>
-  //     <div className="content-wrapper">
-  //       <ContentWrapperComponent />
-  //     </div>
-  //      <div className="footer-container">
-  //         <FooterWrapper />
-  //       </div>
-  //   </div>
-  // </BrowserRouter>
+//   <div className="App">
+//       <div className="top-bar-container">
+//         <TopBarNav />
+//       </div>
+//     <div className="content-wrapper">
+//       <ContentWrapperComponent />
+//     </div>
+//      <div className="footer-container">
+//         <FooterWrapper />
+//       </div>
+//   </div>
+// </BrowserRouter>
--- src/components/TopBar/TopBarNav.js
+++ src/components/TopBar/TopBarNav.js
@@ -1,30 +1,45 @@
-// import "./TopBarNavStyles.scss";
+import "./TopBarNavStyles.scss";
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-// import logo from "../../images/sitelogo2.png";
+import logo from "../../images/sitelogo2.png";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function TopBarNav() {
   const navbarStyle = {
-    backgroundColor: "#00A98F",
+    // backgroundColor: "#00A98F",
   };
-
 
   return (
     <div className="nav-bar-container">
-      <div style={navbarStyle} data-bs-theme="dark">
-        <div className="container">
-          <div className="nav logo">
-            {/* <Link to="/">
-               <img src={logo} alt="LOGO" className="nav-logo" />
-             </Link> */}
-          </div>
-          <div className="top-bar-nav">
-            <Link to="/">Home</Link>
-            <Link to="/about">Projects</Link>
-            <Link to="/contact">Info</Link>
-          </div>
-        </div>
-      </div>
+      <Navbar  data-bs-theme="dark">
+        <Container className="container">
+          <Navbar.Brand className="nav logo">
+            <Nav.Link to="/">
+               <img src={logo} alt="LOGO" className="nav-logo font-class" />
+             </Nav.Link>
+          </Navbar.Brand>
+          <Nav className="top-bar-nav">
+             <Nav.Link to="/" >Home</Nav.Link> {/* className="nav-link font-class" for other font styles*/} 
+            <NavDropdown title="Projects" id="basic-nav-dropdown" className="nav-dropdown">
+              <NavDropdown.Item as="div" className="dropdown-item">
+                <Nav.Link to="/hotels">Hotels</Nav.Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item as="div" className="dropdown-item">
+                <Nav.Link to="/houses">Houses</Nav.Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item as="div" className="dropdown-item">
+                <Nav.Link to="/stores">Stores</Nav.Link>
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link to="/info">Info</Nav.Link>
+            <Nav.Link to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
