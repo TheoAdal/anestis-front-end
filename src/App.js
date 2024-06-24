73737b39eb19e4cf4c5331b2c5df57c0ee2f5d63
TheoAdal <theoadal@gmail.com>
2024-06-24 13:15:47 +0300

    added Loading screen but it is a bit junky

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./App.scss";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import LoadingScreen from "./components/LoadingScreenPage/LoadingScreen";
 import TopBarNav from "./components/TopBar/TopBarNav";
-import FooterWrapper from "./components/Footer/FooterWrapper";
 import ContentWrapper from "./components/ContentWrapper/ContentWrapper";
+import FooterWrapper from "./components/Footer/FooterWrapper";
 
+const App = () => { //KATHE FORA POU KANEI RELOAD BGAINEI TO LOADING
+  const [isLoading, setIsLoading] = useState(true);
 
-function App() {
+  useEffect(() => {
+    // loading time?
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000); //duration of the loading screen
+              //After 5 seconds, page is shown
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="App">
         <div className="top-bar-container">
-          <TopBarNav/>
+          {/* <TopBarNav /> */}
+          {isLoading ? <LoadingScreen /> : <TopBarNav />}
         </div>
         <div className="content-wrapper">
-          <ContentWrapper/> 
+          <ContentWrapper />
+          {/* {isLoading ? <LoadingScreen /> : <ContentWrapper />} */}
         </div>
         <div className="footer-container">
-          <FooterWrapper/>
+          <FooterWrapper />
         </div>
       </div>
     </BrowserRouter>
--- src/components/ContentWrapper/ContentWrapper.js
+++ src/components/ContentWrapper/ContentWrapper.js
@@ -5,16 +5,16 @@ import "./ContentWrapperStyles.scss";
 import Home from "../HomePage/Home";
 import InfoPage from "../InfoPage/Info";
 import Contact from "../ContactPage/Contact";
-
+import LoadingScreen from "../LoadingScreenPage/LoadingScreen";
 
 function ContentWrapperComponent() {
   return (
     <div className="content-wrapper-container">
       <div className="content-wrapper-routes">
         <Routes>
-          <Route exact path="/" element={< Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/info" element={<InfoPage />} />
-          <Route path="/contact" element={<Contact />} /> 
+          <Route path="/contact" element={<Contact />} />
           {/*<Route path="/projects" element={<Projects />} />
           <Route path="/projects/hotels" element={<Hotels />} />
           <Route path="/projects/houses" element={<Houses />} />
