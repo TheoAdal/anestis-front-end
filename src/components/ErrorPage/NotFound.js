a1c99fcfae8f74c2f184efdd717aee3487e0bae8
TheoAdal <theoadal@gmail.com>
2024-06-25 12:30:45 +0300

    added 404 page for faulty urls

---
+++ src/components/ErrorPage/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFoundStyles.scss';
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>Oops! That page can’t be found.</h1>
+      <p>The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+    </div>
+  );
+};
+
+export default NotFound;
