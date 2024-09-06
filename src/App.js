f389abd42178b4f3842ab23f469eb690b33467c0
Eleftherios Adalakis <adalakis.el@gmail.com>
2024-09-06 19:29:49 +0300

    added and deployed to firebase. Created env file for api keys

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
@@ -7,11 +7,11 @@ import TopBarNav from "./components/TopBar/TopBarNav";
 import ContentWrapper from "./components/ContentWrapper/ContentWrapper";
 import FooterWrapper from "./components/Footer/FooterWrapper";
 
-import './i18n.js';
-import LanguageDetectorComponent from './LanguageDetectorComponent'; // Import the language detector component
+import "./i18n.js";
+import LanguageDetectorComponent from "./LanguageDetectorComponent"; // Import the language detector component
 
-
-const App = () => { //KATHE FORA POU KANEI RELOAD BGAINEI TO LOADING
+const App = () => {
+  //KATHE FORA POU KANEI RELOAD BGAINEI TO LOADING
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -19,30 +19,30 @@ const App = () => { //KATHE FORA POU KANEI RELOAD BGAINEI TO LOADING
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 1000); //duration of the loading screen
-              //After 5 seconds, page is shown
+    //After 5 seconds, page is shown
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <BrowserRouter>
-    <LanguageDetectorComponent>
-      <div className="App">
-        <div className="top-bar-container">
-          {/* <TopBarNav /> */}
-          {isLoading ? <LoadingScreen /> : <TopBarNav />}
+      <LanguageDetectorComponent>
+        <div className="App">
+          <div className="top-bar-container">
+            {/* <TopBarNav /> */}
+            {isLoading ? <LoadingScreen /> : <TopBarNav />}
+          </div>
+          <div className="content-wrapper">
+            <ContentWrapper />
+            {/* {isLoading ? <LoadingScreen /> : <ContentWrapper />} */}
+          </div>
+          <div className="footer-container">
+            <FooterWrapper />
+          </div>
         </div>
-        <div className="content-wrapper">
-          <ContentWrapper />
-          {/* {isLoading ? <LoadingScreen /> : <ContentWrapper />} */}
-        </div>
-        <div className="footer-container">
-          <FooterWrapper />
-        </div>
-      </div>
       </LanguageDetectorComponent>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
