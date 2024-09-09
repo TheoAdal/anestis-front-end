f02fb38d1dd7305a3ccf569bdc7bb411b285c227
Eleftherios Adalakis <adalakis.el@gmail.com>
2024-09-09 14:28:13 +0300

    css changes

--- src/components/Footer/FooterWrapper.js
+++ src/components/Footer/FooterWrapper.js
@@ -1,24 +1,28 @@
 import React from "react";
 import "./FooterStyles.scss";
-import logo from "../../images/sitelogo2.png"; //logo
-import { FaPhoneAlt, FaEnvelope } from 'react-icons/fa'; 
+import logo from "../../images/logo512.png"; //logo
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 
 function FooterWrapper() {
   return (
     // <div className="footer-container">
-      <div className="footer-content">
-        <div className="company-info">
-          <img src={logo} alt="Company Logo" className="company-logo" />
-          <div className="name-address">
-            <h4>Anestis Adalakis</h4>
-            <p>Καραισκάκη 9 & Ειρήνης</p>
-          </div>
-        </div>
-        <div className="contact-info">
-        <p><FaPhoneAlt /> +30 2551031277</p>
-        <p><FaEnvelope /> aadalakis@yahoo.gr</p>
+    <div className="footer-content">
+      <div className="company-info">
+        <img src={logo} alt="Company Logo" className="company-logo" />
+        <div className="name-address">
+          <h4>Anestis Adalakis</h4>
+          <p>Καραισκάκη 9 & Ειρήνης</p>
         </div>
       </div>
+      <div className="contact-info">
+        <p>
+          <FaPhoneAlt /> +30 2551031277
+        </p>
+        <p>
+          <FaEnvelope /> aadalakis@yahoo.gr
+        </p>
+      </div>
+    </div>
     // </div>
   );
 }
--- src/components/TopBar/TopBarNav.js
+++ src/components/TopBar/TopBarNav.js
@@ -1,7 +1,7 @@
 import "./TopBarNavStyles.scss";
 import React from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
-import logo from "../../images/sitelogo2.png";
+import logo from "../../images/logo512.png";
 
 import { useTranslation } from "react-i18next";
 
@@ -9,9 +9,11 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
 
 function TopBarNav() {
   const { i18n } = useTranslation();
+  const { t } = useTranslation();
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -34,7 +36,7 @@ function TopBarNav() {
           </Navbar.Brand>
           <Nav className="top-bar-nav">
             <Link className="link" to="/">
-              Home
+              {t("header.home")}
             </Link>
             {/* className="nav-link font-class" for other font styles*/}
             <NavDropdown
@@ -44,20 +46,20 @@ function TopBarNav() {
               style={{ color: "#e7dbdb" }}
             >
               <NavDropdown.Item className="dropdown-item">
-                <Link to="/projects/hotels">Hotels</Link>
+                <Link to="/projects/hotels">{t("header.projects.hotels")}</Link>
               </NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item">
-                <Link to="/projects/houses">Houses</Link>
+                <Link to="/projects/houses">{t("header.projects.houses")}</Link>
               </NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item">
-                <Link to="/projects/stores">Stores</Link>
+                <Link to="/projects/stores">{t("header.projects.stores")}</Link>
               </NavDropdown.Item>
             </NavDropdown>
             <Link className="link" to="/info">
-              Info
+              {t("header.info")}
             </Link>
             <Link className="link" to="/contact">
-              Contact
+              {t("header.contact")}
             </Link>
             {/* DEV */}
             <Link className="link" to="/18927358297659876345987263">
@@ -66,9 +68,9 @@ function TopBarNav() {
           </Nav>
         </Container>
         <div className="language-toggle-btn">
-        <button  onClick={toggleLanguage}>
+          <Button onClick={toggleLanguage} variant="light">
             {currentLanguage === "en" ? "EN" : "GR"}
-          </button>
+          </Button>
         </div>
       </Navbar>
     </div>
