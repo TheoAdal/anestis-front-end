e7852d8507855665ee020127e8917da8996a6a11
TheoAdal <theoadal@gmail.com>
2024-11-04 17:53:28 +0200

    local changes

--- src/components/ContentWrapper/ContentWrapper.js
+++ src/components/ContentWrapper/ContentWrapper.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./ContentWrapperStyles.scss";
 
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 import Home from "../HomePage/Home";
 import InfoPage from "../InfoPage/Info";
@@ -10,14 +10,14 @@ import Contact from "../ContactPage/Contact";
 import ProjectsGrid from "../Projects/ProjectGrid";
 import SpecificProject from "../Projects/SpecificProject";
 
-// import LoadingScreen from "../LoadingScreenPage/LoadingScreen";
-import NotFound from "../ErrorPage/NotFound";
+import LoadingScreen from "../LoadingScreenPage/LoadingScreen";
+import NotFound from "../ErrorPage/NotFound"; 
 import UploadProject from "../UploadProjectPage/UploadCompleteProject";
 import ProjectEditList from "../EditProjectPage/ProjectEditList";
 import EditProject from "../EditProjectPage/EditProject";
 
 function ContentWrapperComponent() {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <div className="content-wrapper-container">
@@ -27,20 +27,10 @@ function ContentWrapperComponent() {
           <Route exact path="/info" element={<InfoPage />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/projects" element={<ProjectsGrid />} />
-          {/* <Route path="/projects/:category" element={<ProjectsGrid />} /> */}
-          <Route
-            path="/projects/:category/:name"
-            element={<SpecificProject />}
-          />
-          <Route path="*" element={<NotFound />} />
-          <Route
-            path="/18927358297659876345987263"
-            element={<UploadProject />}
-          />
-          <Route
-            path="/2034897298456896894587547854"
-            element={<ProjectEditList />}
-          />
+          <Route path="/projects/:name" element={<SpecificProject />} />
+          <Route path="*" element={<NotFound />} /> {/*for undefined routes*/}
+          <Route path="/18927358297659876345987263" element={<UploadProject  />} />
+          <Route path="/2034897298456896894587547854" element={<ProjectEditList />} />
           <Route path="/editproject/:id" element={<EditProject />} />
         </Routes>
       </div>
@@ -48,4 +38,7 @@ function ContentWrapperComponent() {
   );
 }
 
+
+
+          
 export default ContentWrapperComponent;
